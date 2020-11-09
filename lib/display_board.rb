c5a2175622313ad23d5a53e5dc9d0f7d77814105
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-09 04:07:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts "board[0] | board[1]"
+  puts "-----------"
+  puts "board[2] | board[3]"
+  puts "-----------"
+  puts "board[4] | board[5]"
+end
